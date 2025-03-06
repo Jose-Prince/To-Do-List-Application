65f346610e64d28bffe1a1bbffe5b5aef179fd34
Jose-Prince <joseprince148@gmail.com>
2025-03-06 17:12:58 -0600

    style: login form

--- todoapp/src/views/login.tsx
+++ todoapp/src/views/login.tsx
@@ -1,7 +1,26 @@
-
+import {TextField, Button, Typography} from "@mui/material/"
+import "./login.css"
 
 export default function Login() {
     return(
-        <div>5</div>
+        <div className="blur-effect">
+            <div className="container-elements">
+                <Typography variant="h3" gutterBottom
+                    sx = {{
+                        color: "black"
+                    }}    
+                >
+                    To Do List
+                </Typography>
+                <TextField 
+                    id="user_login" 
+                    label="Email" 
+                    variant="outlined"
+                    sx={{
+                        backgroundColor: "white"
+                    }}/>
+                <Button variant="contained">Start</Button>
+            </div>
+        </div>
     )
 }
