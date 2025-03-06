bc36cf320e1bf197952bac895f8106fcc2b02715
Jose-Prince <joseprince148@gmail.com>
2025-03-06 16:23:15 -0600

    build: create Vite + React project with nix flake setup

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Flake for Vite + React";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_18  # O la versión que necesites
+            yarn        # O usa npm si prefieres
+            corepack    # Para manejar versiones de Yarn con `yarn set version stable`
+          ];
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "vite-react-app";
+          src = ./.;
+          buildInputs = with pkgs; [ nodejs_18 yarn ];
+
+          buildPhase = ''
+            yarn install
+            yarn build
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r dist/* $out/
+          '';
+        };
+      }
+    );}
